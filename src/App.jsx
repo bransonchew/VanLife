d820aea6f4f2a695d12a65a8ff81258e3fa6e121
bransonchew <99803923+bransonchew@users.noreply.github.com>
2023-06-20 00:18:47 +0800

    Host routes & pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,28 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './pages/Home'
 import About from './pages/About'
-import Vans from './pages/Vans'
-import Van from './pages/Van'
-import '../server'
+import Vans from './pages/vans/Vans'
+import Van from './pages/vans/Van'
+import Dashboard from './pages/host/Dashboard'
+import Income from './pages/host/Income'
+import Reviews from './pages/host/Reviews'
+import HostLayout from './components/HostLayout'
 
 
 export default function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route element={ <Layout/> }>
-                    <Route path="/" element={ <Home/> }/>
-                    <Route path="/about" element={ <About/> }/>
-                    <Route path="/vans" element={ <Vans/> }/>
-                    <Route path="/vans/:id" element={ <Van/> }/>
+                <Route path="/" element={ <Layout/> }>
+                    <Route index element={ <Home/> }/>
+                    <Route path="about" element={ <About/> }/>
+                    <Route path="vans" element={ <Vans/> }/>
+                    <Route path="vans/:id" element={ <Van/> }/>
+                    <Route path="host" element={ <HostLayout/> }>
+                        <Route index element={ <Dashboard/> }/>
+                        <Route path="income" element={ <Income/> }/>
+                        <Route path="reviews" element={ <Reviews/> }/>
+                    </Route>
                 </Route>
             </Routes>
         </BrowserRouter>
